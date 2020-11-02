6502ca6397cec610a630772a37221ac11997892c
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-11-02 12:12:21 -0800

    css and deleted page link

--- src/Header.js
+++ src/Header.js
@@ -5,19 +5,18 @@ import { Link } from 'react-router-dom';
 export default class Header extends Component {
     render() {
         return (
-            <div className='header-title'>
-                Search your Pokedex!
-            <ul className='header-link-list'> 
-                <li>
-                <Link to="/" >Home</Link>
-                </li>
-                <li>
-                <Link to="/list" >List</Link>
-                </li>
-                <li>
-                <Link to="/pagination" >Pages</Link>
-                </li>
-            </ul>
+            <div className='header-div'>
+                <div className='header-title'>
+                    Search your Pokedex!
+                <ul className='header-link-list'> 
+                    <li>
+                    <Link to="/" >Home</Link>
+                    </li>
+                    <li>
+                    <Link to="/list" >List</Link>
+                    </li>
+                </ul>
+                </div>
             </div>
         )
     }
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
+            <div className='welcome'>
                 Welcome to the Pokedex!
             </div>
         )
